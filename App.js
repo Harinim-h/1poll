c2909d8539723471d110033172ea3f1b10cf377e
Harinim-h <harinim378@gmail.com>
2024-12-06 19:32:53 +0530

    suveys

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+   import Polls from './components/Polls';
+
+   const App = () => <Polls />;
+
+   export default App;
+   import { reportWebVitals } from 'web-vitals';
+
+   reportWebVitals();
